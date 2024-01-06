3a88b506ef32e882cf30d0f32385a787f702878f
Tewodros Misawoy <118480807+Tewodros01@users.noreply.github.com>
2024-01-05 23:29:36 -0800

    Update app.config.ts

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,7 +1,7 @@
 const MONGO_DB_CONFIG = {
   DB: "mongodb+srv://tewodrosm01:1992202424@cluster0.ixlmrr8.mongodb.net/?retryWrites=true&w=majority",
   PAGE_SIZE: 10,
-  BCRYPT_PASSWORD: "Grocery App",
+  BCRYPT_PASSWORD: "EcommersApp",
   SALT_ROUND: 10,
   TOKEN_SECRET: 2119,
 };
